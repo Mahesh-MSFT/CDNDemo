02ce7b84759a966e33f987259f51e1d63aa6884d
Mahesh-MSFT <maksh@microsoft.com>
2017-09-13 13:22:18 +0530

    Add project files.

---
+++ CDNDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CDNDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public int starttime { get; set; }
+        public int endtime { get; set; }
+        public void OnGet()
+        {
+            starttime = DateTime.Now.Millisecond;
+            endtime = DateTime.Now.Millisecond;
+        }
+
+        public void OnPost()
+        {
+            starttime = DateTime.Now.Millisecond;
+
+            ViewData["Image1Path"] = "/images/ws1.jpg";
+            ViewData["Image2Path"] = "/images/ws2.jpg";
+            ViewData["Image3Path"] = "/images/ws3.jpg";
+            ViewData["Image4Path"] = "/images/ws4.jpg";
+            ViewData["Image5Path"] = "/images/ws5.jpg";
+            ViewData["Image6Path"] = "/images/ws6.jpg";
+
+            endtime = DateTime.Now.Millisecond;
+        }
+    }
+}
